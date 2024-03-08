b2b17688722e6b706648d9b8de97d361308037e2
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-03-08 19:16:38 +0530

    Making shopping list button functional
    Allow user to add new ingredient.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.css'
+  styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', { static: true }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInputRef: ElementRef;
 
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAddItem() {
+    const ingredientName = this.nameInputRef.nativeElement.value;
+    const ingredientAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
