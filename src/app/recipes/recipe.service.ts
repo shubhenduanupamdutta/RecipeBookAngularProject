781779bd2f006b8f028a206a27b78d77add9b6bd
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-03-14 19:21:15 +0530

    Add ingredients to show in details

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable()
 export class RecipeService {
@@ -7,14 +8,24 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2015/10/26/07/21/vegetables-1006694_1280.jpg'
+      'Masala Dosa',
+      'A south indian all time any time dish.',
+      'https://media.gettyimages.com/id/1212684210/photo/south-indian-breakfast-dosa.jpg?s=2048x2048&w=gi&k=20&c=tgIqvK0z37fMZCPadGgAlLmTKJtjxP7WgU1PVMmGeCE=',
+      [
+        new Ingredient('Rice', 1),
+        new Ingredient('Potato', 2),
+        new Ingredient('Onion', 1),
+      ]
     ),
     new Recipe(
-      'A Test Recipe2',
-      'This is simply a test2',
-      'https://cdn.pixabay.com/photo/2015/10/26/07/21/vegetables-1006694_1280.jpg'
+      'Samosa',
+      'A ubiquitous north indian snack.',
+      'https://media.gettyimages.com/id/1317460663/photo/close-up-of-fried-food-on-table-west-chester-pennsylvania-united-states-usa.jpg?s=2048x2048&w=gi&k=20&c=K0RXgb4QQDQz8VoCqUuVFq9N9b0CcgbCMKSaFge2J7g=',
+      [
+        new Ingredient('Potato', 2),
+        new Ingredient('Peas', 1),
+        new Ingredient('Flour', 1),
+      ]
     ),
   ];
 
