0207098a296fa1fb496a12014aea119d3be4a6a8
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-03-14 19:31:15 +0530

    Using services to pass data around the app.
    Used recipe service in conjunction with shopping-list service to add ingredients of a recipe direct to shopping list.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
@@ -29,7 +30,13 @@ export class RecipeService {
     ),
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
